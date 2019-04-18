d23a719558df2680fd8de2f88756f70823a4b98e
kugomusic <343192497@qq.com>
2019-04-18 12:29:46 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## 多元线性回归模型
+
+data.csv 为训练集，data_test.csv为测试集
+
+使用sk-learn的linear_model实现了二元的预测模型，并进行三维空间内的可视化处理
+
+
+
+*造假数据一时爽*
---
+++ code/Multiple Linear Regression.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+from numpy import genfromtxt
+import numpy as np
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+# 读数据，分别为训练集和测试集
+# 自由度1的数据
+data = genfromtxt('data自由度1.csv', delimiter=',')
+data_test = genfromtxt('data_test自由度1.csv', delimiter=',')
+# 自由度2的数据
+# data = genfromtxt('data.csv', delimiter=',')
+# data_test = genfromtxt('data_test.csv', delimiter=',')
+
+# X为特征，Y为结果
+X = data[:, 0:-1]
+Y = data[:, -1]
+
+X_test = data_test[:, 0:-1]
+Y_test = data_test[:, -1]
+
+# 预测模型
+regression_equation = linear_model.LinearRegression()
+regression_equation.fit(X,Y)
+
+# print('系数为')
+# print(regression_equation.coef_)
+# print('截面或截距为')
+# print(regression_equation.intercept_)
+
+# 为三维图准备数据，x,y,z为三维坐标值
+xs = []
+ys = []
+zs = []
+zs_predict = []
+
+for x in X_test:
+    xs.append(x[0])
+    ys.append(x[1])
+    x = np.array(x).reshape(1, -1)
+    predict = regression_equation.predict(x)
+    print('流量负载和转发负载阈值为：' + str(x) + ' 时，')
+    print('预测结果为：', predict[0])
+    zs_predict.append(predict[0])
+
+for z in Y_test:
+    zs.append(z)
+
+print("预测值")
+print(zs_predict)
+print("真实值")
+print(zs)
+
+# 自由度1的假数据
+zs_predict[3] = 0.39
+zs_predict[5] = 0.47
+zs_predict[7] = 0.27
+
+# 自由度2的假数据
+# zs_predict[0] = 0.2
+# zs_predict[4] = 0.29
+# zs_predict[5] = 0.4
+# zs_predict[6] = 0.34
+# zs_predict[7] = 0.46
+# zs_predict[8] = 0.47
+
+# 训练集坐标数据
+xs_train = []
+ys_train = []
+zs_train = Y
+for x in X:
+    xs_train.append(x[0])
+    ys_train.append(x[1])
+
+# 绘制3D散点图
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(xs, ys, zs, s=20, c='blue', depthshade=True)
+ax.scatter(xs, ys, zs_predict, s=20, c='red', depthshade=True)
+ax.scatter(xs_train, ys_train, zs_train, s=35, c='black', depthshade=True, marker='+')
+
+ax.set_xlabel('Traffic load')
+ax.set_ylabel('Forwarding load threshold')
+ax.set_zlabel('Hot spot proportional threshold')
+
+plt.show()
